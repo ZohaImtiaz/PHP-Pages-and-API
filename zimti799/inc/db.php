9eea0d3977e7d6c019e4a782026f50027f9450db
Zimti799 <157741130+ZohaImtiaz@users.noreply.github.com>
2025-10-20 07:17:36 -0600

    Update db.php

--- zimti799/inc/db.php
+++ zimti799/inc/db.php
@@ -4,26 +4,21 @@
 
 /* */
 /*directory path to current file.*/
-$dbPath = __DIR__ . '/../data/stocks.db'; 
+$dbPath = __DIR__ . '/../data/stocks.db';
 
-/* if file doesnt exits at this path*/
 if (!file_exists($dbPath)) {
-    //show error mssg
     http_response_code(500);
-    echo "Database file not found at: " . htmlspecialchars($dbPath);
+    echo "Database file not found: " . htmlspecialchars($dbPath);
     exit;
 }
 
-
 try {
-    //object tells which database file to open
     $pdo = new PDO('sqlite:' . $dbPath);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    //internat server error mssf if try block fails 
     http_response_code(500);
-     // Output a user safe version of the error message.
-    echo "DB connection failed: " . htmlspecialchars($e->getMessage());
+    echo "Database connection failed: " . htmlspecialchars($e->getMessage());
     exit;
 }
+?>
