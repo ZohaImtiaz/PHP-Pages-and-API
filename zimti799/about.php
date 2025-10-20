13b1da2100731244c89c3c7512c4229f54eebb62
Zimti799 <157741130+ZohaImtiaz@users.noreply.github.com>
2025-10-20 03:13:28 -0600

    Update about.php

--- zimti799/about.php
+++ zimti799/about.php
@@ -4,26 +4,24 @@ include __DIR__ . '/inc/header.php';
 <section class="container about">
   <h2>About this site</h2>
 
-  <p>This project is <strong>Assignment #1 for COMP3512</strong> at Mount Royal University. The site uses PHP + PDO with an SQLite database to display company and historical stock data. It also provides JSON APIs for later use in Assignment #2.</p>
+  <p>This project is <strong>Assignment #1 for COMP3512</strong> at Mount Royal University. The site uses PHP + PDO with an SQLite database to display company and historical stock data. </p>
 
   <h3>Authors / Team</h3>
   <ul>
-    <li>Your Name — youruserid</li>
-    <!-- Add partner(s) here -->
+    <li>Zoha Imtiaz</li>
   </ul>
 
   <h3>Technologies</h3>
   <ul>
     <li>PHP (with PDO)</li>
     <li>SQLite</li>
-    <li>HTML & CSS (no JavaScript for this assignment)</li>
+    <li>HTML & CSS</li>
   </ul>
 
   <h3>GitHub</h3>
-  <p>Repository URL: <a href="https://github.com/yourusername/yourrepo" target="_blank">https://github.com/yourusername/yourrepo</a></p>
+  <p>Repository URL: <a href="https://github.com/ZohaImtiaz/PHP-Pages-and-API" target="_blank">https://github.com/yourusername/yourrepo</a></p>
 
   <h3>Notes to marker</h3>
-  <p>APIs are placed in <code>/api/</code>. Database (SQLite) is in <code>/data/</code>. If I used any third-party CSS, I have credited it in <code>/css/style.css</code>.</p>
 </section>
 
 </main>
