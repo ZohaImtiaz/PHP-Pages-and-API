288d603fc2b9633fab6557a9568968a144ee2d78
Zimti799 <157741130+ZohaImtiaz@users.noreply.github.com>
2025-10-19 19:53:45 -0600

    Create header.php

---
+++ zimti799/inc/header.php
@@ -0,0 +1,21 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>COMP3512 Stocks</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <header class="site-header">
+    <div class="container header-inner">
+      <h1 class="brand"><a href="/index.php">COMP3512  Stock Viewer</a></h1>
+      <nav class="nav">
+        <a href="/index.php">Home</a>
+        <a href="/about.php">About</a>
+        <a href="/api_tester.php">API Tester</a>
+      </nav>
+    </div>
+  </header>
+  <main>
