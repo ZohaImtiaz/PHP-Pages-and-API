4557fd04c7dae00ec2f0687b8552f4480717445e
Zimti799 <157741130+ZohaImtiaz@users.noreply.github.com>
2025-10-20 05:06:33 -0600

    Update header.php

--- zimti799/inc/header.php
+++ zimti799/inc/header.php
@@ -8,16 +8,16 @@ $baseURL = '/zimti799/';
   <meta charset="utf-8">
   <title>COMP3512 Stocks</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="<?= $baseURL ?>css/style.css">
 </head>
 <body>
   <header class="site-header">
     <div class="container header-inner">
-      <h1 class="brand"><a href="/index.php">COMP3512  Stock Viewer</a></h1>
+      <h1 class="brand"><a href="<?= $baseURL ?>index.php">COMP3512 - Stock Viewer</a></h1>
       <nav class="nav">
-        <a href="/index.php">Home</a>
-        <a href="/about.php">About</a>
-        <a href="/api_tester.php">API Tester</a>
+        <a href="<?= $baseURL ?>index.php">Home</a>
+        <a href="<?= $baseURL ?>about.php">About</a>
+        <a href="<?= $baseURL ?>api_tester.php">API Tester</a>
       </nav>
     </div>
   </header>
